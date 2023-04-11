d4f1656638d35cad28986c3fc7ebd62f289acc98
zerotymer+github@gmail.com <zerotymer+github@gmail.com>
2023-04-11 16:28:45 +0900

    2023-04-11

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # build
-echo "javac -classpath lib/*:src/* -d target $(find ./src -name *.java)"
-javac -classpath lib/*:src/* -d target "$(find ./src -name *.java)"
+# make file list
+echo "find src/$1 -name *.java > sources.txt"
+find src/$1 -name *.java > sources.txt
+echo "javac -classpath lib/* -d target @sources.txt"
+javac -classpath lib/* -d target @sources.txt
 #javac-algs4 -d target ./src/$1/$2/*.java 
 
 # run
-echo "java -classpath target:lib/args4.jar $1.$2.$3 $4"
-echo ""
-echo ""
-java -classpath target:lib/* $1.$2.$3 $4
+echo "java -classpath target:lib/args4.jar $2 $3"
+java -classpath target:lib/* $2 $3 
 #java-algs4 -classpath target $1.$2.$3 $4
